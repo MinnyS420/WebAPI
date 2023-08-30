42c952808db0e58e597e517f15f0e403e943634f
mstamenkovski27@gmail.com <mstamenkovski27@gmail.com>
2023-08-30 22:25:19 +0200

    MoviesApp - Refactored

---
+++ MoviesApp - Refactored/MoviesApp/MoviesApp.DataAccess/MoviesAppDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.Domain;
+
+namespace MoviesApp.DataAccess
+{
+    public class MoviesAppDbContext : DbContext
+    {
+        public MoviesAppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed
+
+            modelBuilder.Entity<Movie>()
+                .HasData(new Movie
+                {
+                    Id = 1,
+                    Title = "Foo",
+                    Description = "Bar",
+                    Genre = Domain.Enums.Genre.Action,
+                    Year = 2014,
+                });
+
+            modelBuilder.Entity<Movie>()
+                .HasData(new Movie
+                {
+                    Id = 2,
+                    Title = "Boo",
+                    Description = "Bar",
+                    Genre = Domain.Enums.Genre.Thriller,
+                    Year = 2015,
+                });
+
+            modelBuilder.Entity<Movie>()
+                .HasData(new Movie
+                {
+                    Id = 3,
+                    Title = "Hoo",
+                    Description = "Bar",
+                    Genre = Domain.Enums.Genre.Comedy,
+                    Year = 2016,
+                });
+        }
+    }
+}
---
+++ MoviesApp - Refactored/MoviesApp/MoviesApp.Services/Abstraction/IMovieSevice.cs
@@ -0,0 +1,17 @@
+﻿using MoviesApp.Domain;
+using MoviesApp.Domain.Enums;
+using MoviesApp.DTOs;
+
+namespace MoviesApp.Services.Abstraction
+{
+    public interface IMovieSevice
+    {
+        List<MovieDTO> GetAllMovies();
+        MovieDTO GetById(int id);
+        List<string> GetTitlesFromMovies(List<Movie> movies);
+        List<MovieDTO> GetMoviesByGenreAndYear (Genre? genre, int? year);
+        void AddMovie(MovieDTO addMovieDto);
+        void UpdateMovie(UpdateMovieDTO updateMovieDto);
+        void DeleteMovie(int id);
+    }
+}
