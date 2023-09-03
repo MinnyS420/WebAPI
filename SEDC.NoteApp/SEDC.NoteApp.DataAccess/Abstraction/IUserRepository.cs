9e9fd41e8097c2e3720544c11fcde3194aeee0ed
mstamenkovski27@gmail.com <mstamenkovski27@gmail.com>
2023-09-04 00:04:38 +0200

    NoteApp - Updated(Change password)

---
+++ SEDC.NoteApp/SEDC.NoteApp.DataAccess/Abstraction/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.NoteApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.NoteApp.DataAccess.Abstraction
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User GetUserByUsername(string username);
+        User LoginUser(string username, string hashedPassword);
+        void ChangePassword(User user);
+    }
+}
---
+++ SEDC.NoteApp/SEDC.NoteApp.Services/Abstraction/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.NoteApp.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.NoteApp.Services.Abstraction
+{
+    public interface IUserService
+    {
+        void RegisterUser(RegisterUserDto registerUserDto);
+        string LoginUser(LoginUserDto loginUserDto);
+        string ChangePasswordUser(ChangePasswordUserDto changePasswordUserDto);
+    }
+}
